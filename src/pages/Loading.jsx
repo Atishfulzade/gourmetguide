677f5cf9505c04a67b7d1b5b88ba1ab1b2f97dd6
Atishfulzade <atishfulzade1234@gmail.com>
2024-07-15 12:27:34 +0530

    bckground

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center w-full h-[100vh] text-gray-900 dark:text-gray-100 dark:bg-gray-950">
+    <div className="flex bg-white items-center justify-center w-full h-[100vh] text-gray-900 dark:text-gray-100 dark:bg-gray-950">
       <div>
         <h1 className="text-xl md:text-5xl cursive text-red-500 animate-bounce flex gap-2 items-center">
           Gurnet
